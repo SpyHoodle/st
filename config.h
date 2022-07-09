5c5da24154b1d0fd56c14832d791ebcfd4a9c93b
Madeleine <spyhoodle@gmail.com>
2022-07-09 14:04:31 +0100

    change font to Iosevka

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = {
 	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
 	"Font Awesome 6 Free Solid:pixelsize=16:antialias=true:autohint=true",
